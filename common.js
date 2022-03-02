037ee330df7030c21f5cd90cf1a810be72ac0bf3
Sitthiporn <2020511101002@live4.utcc.ac.th>
2022-03-02 14:56:21 +0700

    Refractoring#3 modify return message

--- common.js
+++ common.js
@@ -28,5 +28,5 @@ function calculate(p,d){
 		var ft = finishtime(pace,dist);
 	//---------------------------
 		msg = "successfully calculated!"
-		return {"status":1,"message":msg,"hour":hour,"min":min};
+		return {"status":1,"message":msg,"hour":ft.hour,"min":ft.min};
 }
