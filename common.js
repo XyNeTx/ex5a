7b55e8a3c2f7be9bfdc2dfb71b8250b49f690bc3
Sitthiporn <2020511101002@live4.utcc.ac.th>
2022-03-02 14:49:49 +0700

    Refractoring#2 add finishTime fn

--- common.js
+++ common.js
@@ -1,5 +1,4 @@
-function validate(p)
-	{
+function validate(p){
 		if(isNaN(pace)){
 			msg = "Please verify that the pace you have entered is correct!!!";
 			return {"status":0,"message":msg,"hour":0,"min":0};
@@ -8,8 +7,16 @@ function validate(p)
 			msg = "Running pace must be between 3 and 10 min/km";
 			return {"status":0,"message":msg,"hour":0,"min":0};
 		}
-	}
-
+}
+function finishtime(p,d){
+	var min = dist * pace;
+		var hour = 0;
+		if(min > 60){
+			hour = Math.floor(min / 60);          
+			min = min % 60;
+		}
+		return {"hour":hour,"min":min};
+}
 function calculate(p,d){
 		var msg = "";
 		var pace = parseFloat(p);
@@ -18,12 +25,7 @@ function calculate(p,d){
 		var chk = validate(pace);
 		if(chk != null)return chk;
 	//---------------------------
-		var min = dist * pace;
-		var hour = 0;
-		if(min > 60){
-			hour = Math.floor(min / 60);          
-			min = min % 60;
-		}
+		var ft = finishtime(pace,dist);
 	//---------------------------
 		msg = "successfully calculated!"
 		return {"status":1,"message":msg,"hour":hour,"min":min};
